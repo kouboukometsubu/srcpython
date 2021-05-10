a50e651a191fcca89bb92746c3e3da02d76c91c4
hsaito <saitoh@takano-net.co.jp>
2021-05-10 22:14:14 +0900

    1

--- trainning/dirListFile.py
+++ trainning/dirListFile.py
@@ -4,7 +4,7 @@ import os
 # Headerファイルのみを取得
 files = glob.glob("D:\srcfile\AltaxV4Comm\Std\DLLTools\StdClass\cpphtools\*.h")
 
-
+#ファイル名だけを出力
 for file in files:
     filename = os.path.basename(file)
     print(filename)
